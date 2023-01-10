2867bb8f91e81e5381a5e56e16b2c2411a706033
KMacfarlaneGreen <kmmg2@cam.ac.uk>
2023-01-10 11:23:39 +0000

    reformat

--- src/decentralised_marl/ring_antisocial.py
+++ src/decentralised_marl/ring_antisocial.py
@@ -139,7 +139,7 @@ if __name__ =="__main__":
     parser.add_argument('-fsm', '--first-step-memory', default=0, type=float,
                         help='Number of initial steps for just filling the memory')
     parser.add_argument('-rs', '--replay-steps', default=4, type=float, help='Steps between updating the network')
-    parser.add_argument('-nn', '--number-nodes', default=256, type=int, help='Number of nodes in each layer of NN')
+    #parser.add_argument('-nn', '--number-nodes', default=256, type=int, help='Number of nodes in each layer of NN')
     #parser.add_argument('-tt', '--target-type', choices=['DQN', 'DDQN'], default='DDQN')
     #parser.add_argument('-mt', '--memory', choices=['UER', 'PER'], default='PER')
     #parser.add_argument('-pl', '--prioritization-scale', default=0.5, type=float, help='Scale for prioritization')
