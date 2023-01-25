252caec59aafd9b99bb1b28d360b1080f9855240
KMacfarlaneGreen <kmmg2@cam.ac.uk>
2023-01-25 15:07:09 +0000

    edit file creation

--- src/habitat_selection/two_patch_experiment.py
+++ src/habitat_selection/two_patch_experiment.py
@@ -191,27 +191,21 @@ if __name__ =="__main__":
     rewards_path = './habitat_selection/rewards_files/'
     if not os.path.isdir(rewards_path):
         os.makedirs(rewards_path)
-        os.mkdir(rewards_file)
     brain_path = './habitat_selection/weights_files/'
     if not os.path.isdir(brain_path):
         os.makedirs(brain_path)
-        os.mkdir(brain_file)
     timesteps_path = './habitat_selection/timesteps_files/'
     if not os.path.isdir(timesteps_path):
         os.makedirs(timesteps_path)
-        os.mkdir(timesteps_file)
     actions_path = './habitat_selection/actions/'
     if not os.path.isdir(actions_path):
         os.makedirs(actions_path)
-        os.mkdir(action_file)
     states_path = './habitat_selection/states/'
     if not os.path.isdir(states_path):
         os.makedirs(states_path)
-        os.mkdir(states_file)
     rews_path = './habitat_selection/rewards_timesteps/'
     if not os.path.isdir(rews_path):
         os.makedirs(rews_path)
-        os.mkdir(rews_file)
     loss_path = './habitat_selection/losses/'
     if not os.path.isdir(loss_path):
         os.makedirs(loss_path)
